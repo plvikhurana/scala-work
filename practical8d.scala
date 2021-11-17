1c4c7c5cc69945aa4bf066b91b328e7f75f6e0c5
plvikhurana <87965004+plvikhurana@users.noreply.github.com>
2021-11-17 13:55:56 +0530

    Create practical8d.scala

---
+++ practical8d.scala
@@ -0,0 +1,15 @@
+object sort
+{ 
+  
+    // Main method
+    def main(args:Array[String])
+    {
+      
+        // Applying indexOf method
+        val result = "Kashish".indexOf('i')
+          
+        // Displays output
+        println(result)
+      
+    }
+} 
