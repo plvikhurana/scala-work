94b77fe7088708b69959533625e3fe465544a0bf
plvikhurana <87965004+plvikhurana@users.noreply.github.com>
2021-11-17 13:55:03 +0530

    Create practical8c.scala

---
+++ practical8c.scala
@@ -0,0 +1,18 @@
+object character
+{ 
+  
+    // Main method
+    def main(args:Array[String])
+    {
+      
+        // Applying toCharArray method
+        val result = "KashishMirza".toCharArray()
+          
+        for(m1<-result)
+        {
+            // Displays output
+            println(m1)
+        }
+          
+    }
+}
