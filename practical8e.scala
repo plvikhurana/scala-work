967c3194f17f9dbb18bd8c32c161e8ab9635b432
plvikhurana <87965004+plvikhurana@users.noreply.github.com>
2021-11-17 13:57:13 +0530

    Create practical8e.scala

---
+++ practical8e.scala
@@ -0,0 +1,15 @@
+object Scala_8
+{ 
+  
+    // Main method
+    def main(args:Array[String])
+    {
+      
+        // Applying substring method
+        val result = "PallaviKhurana".substring(8)
+          
+        // Displays output
+        println(result)
+      
+    }
+} 
